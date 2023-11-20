2466cc18986ae0a052230e0097bfb3230ceaa223
orgulec <oggilion@interia.pl>
2023-11-20 18:44:52 +0100

    first version of Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        var isRunning = true;
+        var isFirstRun = true;
+        while (isRunning) {
+            if (isFirstRun) {
+                System.out.println("""
+                        ----------------------
+                        WELCOME!
+                        type X to quit
+                        type Y to get a weather stats
+                        ----------------------
+                        """);
+                isFirstRun = false;
+            }
+            Scanner sc = new Scanner(System.in);
+            String userInput = sc.nextLine();
+
+            switch (userInput) {
+                case "X" -> isRunning = false;
+                case "Y" -> System.out.println("TODO: add some weather data");
+            }
+        }
+
+    }
+
 }

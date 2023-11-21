0688514b382b31920d1cb4e30644e75ad2e9100a
orgulec <oggilion@interia.pl>
2023-11-21 19:39:43 +0100

    CityWeatherDb remove

---
+++ src/test/java/db/CityWeatherDbTest.java
@@ -0,0 +1,23 @@
+package db;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CityWeatherDbTest {
+
+    @Test
+    void addMethodTest() {
+
+    }
+
+    @Test
+    void removeMethodTest() {
+
+    }
+
+    @Test
+    void changeMethodTest() {
+
+    }
+}
